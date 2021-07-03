a03a519262edc1002d521343359a57c4afdf6acb
pangliang <418094911@qq.com>
2021-07-03 10:13:32 +0000

    可设置delay

--- gif.lua
+++ gif.lua
@@ -4,6 +4,14 @@ require 'bit'
 local httpc = require("resty.http").new()
 
 local args = ngx.req.get_uri_args()
+local url = args.url
+if url == nil then
+    ngx.say("args url is empty")
+    return
+end
+
+local delay = args.delay and args.delay/10 or 3
+
 local res, err = httpc:request_uri(args.url, {
     method = "GET",
 })
@@ -25,7 +33,7 @@ end
 function skip(body, p)
     local size = body:byte(p())
     while size ~= 0 do
-        ngx.log(ngx.STDERR, string.format("skip size: %d/%x, p:%x", size, size, (p(0)-1)))
+        -- ngx.log(ngx.STDERR, string.format("skip size: %d/%x, p:%x", size, size, (p(0)-1)))
         p(size)
         size = body:byte(p())
     end
@@ -87,8 +95,6 @@ while p(0) < body:len() do
         ngx.log(ngx.STDERR, string.format("ext_block_flag:%x",ext_block_flag))
         if ext_block_flag == 0xff then
             local bflag = {body:byte(p(), p(0)+15)}
-            ngx.log(ngx.STDERR, "bflag:", string.format("%x,%x,%x,%x", bflag[1],bflag[2],bflag[3],bflag[4]))
-            ngx.log(ngx.STDERR, "bflag:", string.sub(body, p(0), p(0)+20))
             if bflag[1] == 0x0b and string.sub(body, p(0)+1, p(0)+11) == "NETSCAPE2.0" then
                 -- application block
                 ngx.log(ngx.STDERR, "NETSCAPE2.0")
@@ -96,7 +102,6 @@ while p(0) < body:len() do
                 ngx.log(ngx.STDERR, string.format("p:%x", p(0)))
                 local sub_block_data_size = body:byte(p())
                 local sub_block_id = body:byte(p())
-                ngx.log(ngx.STDERR, string.format("sub_block_data_size:%x, sub_block_id:%x", sub_block_data_size, sub_block_id))
                 if sub_block_id == 0x01 then
                     -- loop count
                     local loop_count = body:byte(p())+(body:byte(p())*2^8)
@@ -115,13 +120,18 @@ while p(0) < body:len() do
             local block_size = body:byte(p())
             ngx.log(ngx.STDERR, "block_size:", block_size)
             local pf1 = body:byte(p())
+            -- 设置delay 为 100/0x64
+            body = body:sub(1, p(0)) .. string.char(delay, 0) .. body:sub(p(0)+3) 
             local delay = body:byte(p())+(body:byte(p())*2^8)
             ngx.log(ngx.STDERR, "delay:", delay)
             local tra_color_index = body:byte(p())
             local block_terminator = body:byte(p())
-        elseif f == 0xfe then
+        elseif ext_block_flag == 0xfe then
             -- Comment Extension
             skip(body, p)
+        else
+            ngx.log(ngx.STDERR, string.format("unknow ext_block_flag:%x,p:%x",ext_block_flag, p(0)))
+            break
         end
     elseif block_flag == 0x2c then
         -- 图像标识符(Image Descriptor)
@@ -141,7 +151,7 @@ while p(0) < body:len() do
         break
     else
         ngx.log(ngx.STDERR, string.format("unknow block_flag:%x,p:%x",block_flag, p(0)))
-        return
+        break
     end 
 end
 
